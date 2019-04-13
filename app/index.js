1942062362b708198d205fa90466e0eb18f82d8d
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-04-12 21:07:59 -0400

    Add a ship sprite - Some game config tweaks

--- app/index.js
+++ app/index.js
@@ -5,9 +5,19 @@ import { SimpleScene } from './scenes/SimpleScene';
 import 'styles/index.scss';
 
 const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
+  type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: [
+        SimpleScene
+    ]
 };
 
 new Phaser.Game(gameConfig);
--- app/scenes/SimpleScene.js
+++ app/scenes/SimpleScene.js
@@ -1,5 +1,11 @@
 export class SimpleScene extends Phaser.Scene {
-    create() {
-      this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    }
+
+	preload() {
+		this.load.spritesheet('ship', 'assets/sprites/anim_ship_spin.png', { frameWidth: 191, frameHeight: 63 });
+	}
+
+	create() {
+		this.add.image(400, 300, 'ship');
+		this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+	}
 }
