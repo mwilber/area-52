9d9429c6d1bd72c064c77c3deed8da936004fa8f
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-04-12 20:28:21 -0400

    Set up phaser

--- app/index.js
+++ app/index.js
@@ -8,3 +8,14 @@ import 'styles/index.scss';
 // ================================
 // START YOUR APP HERE
 // ================================
+import 'phaser';
+
+import { SimpleScene } from './scenes/SimpleScene';
+
+const gameConfig = {
+  width: 680,
+  height: 400,
+  scene: SimpleScene
+};
+
+new Phaser.Game(gameConfig);
