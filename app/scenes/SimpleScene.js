bef28a2ff51953fed146489d21ec2108daf43267
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-04-12 21:46:31 -0400

    Playing with gravity and friction and controls

--- app/scenes/SimpleScene.js
+++ app/scenes/SimpleScene.js
@@ -1,5 +1,7 @@
 export class SimpleScene extends Phaser.Scene {
 
+	
+
 	preload() {
 		this.load.spritesheet('ship', 'assets/sprites/anim_ship_spin.png', { frameWidth: 192, frameHeight: 63 });
 		
@@ -8,9 +10,12 @@ export class SimpleScene extends Phaser.Scene {
 	create() {
 		this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
 
-		var player = this.physics.add.sprite(400, 300, 'ship');
+		this.cursors = this.input.keyboard.createCursorKeys();
+
+		this.player = this.physics.add.sprite(400, 300, 'ship');
 		//player.body.setGravity(0,0);
-		player.body.allowGravity = false;
+		this.player.body.allowGravity = false;
+		this.player.setFriction(1);
 
 		this.anims.create({
 			key: 'spin',
@@ -19,6 +24,18 @@ export class SimpleScene extends Phaser.Scene {
 			repeat: -1
 		});
 
-		player.anims.play('spin', true);
+		this.player.anims.play('spin', true);
+	}
+
+	update()
+	{
+		if (this.cursors.left.isDown)
+		{
+			this.player.setVelocityX(-160);
+		}
+		else if (this.cursors.right.isDown)
+		{
+			this.player.setVelocityX(160);
+		}
 	}
 }
