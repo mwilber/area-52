07d53cb4b4349e2434209f2ff4e441a60e446a73
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-04-12 21:36:44 -0400

    clean up spinning ship anim

--- app/scenes/SimpleScene.js
+++ app/scenes/SimpleScene.js
@@ -1,11 +1,24 @@
 export class SimpleScene extends Phaser.Scene {
 
 	preload() {
-		this.load.spritesheet('ship', 'assets/sprites/anim_ship_spin.png', { frameWidth: 191, frameHeight: 63 });
+		this.load.spritesheet('ship', 'assets/sprites/anim_ship_spin.png', { frameWidth: 192, frameHeight: 63 });
+		
 	}
 
 	create() {
-		this.add.image(400, 300, 'ship');
 		this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+
+		var player = this.physics.add.sprite(400, 300, 'ship');
+		//player.body.setGravity(0,0);
+		player.body.allowGravity = false;
+
+		this.anims.create({
+			key: 'spin',
+			frames: this.anims.generateFrameNumbers('ship', { start: 0, end: 3 }),
+			frameRate: 10,
+			repeat: -1
+		});
+
+		player.anims.play('spin', true);
 	}
 }
