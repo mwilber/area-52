9d5a9d50562946a3eea4937dd00e4a0543b686d3
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-04-14 19:27:41 -0400

    Experimentation with mario spritesheet

--- app/scenes/SimpleScene.js
+++ app/scenes/SimpleScene.js
@@ -4,12 +4,52 @@ export class SimpleScene extends Phaser.Scene {
 		super({
 			key: 'SimpleScene'
 		});
+
+		this.background = [
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  1,  2,  3,  0,  0,  0,  1,  2,  3,  0 ],
+			[  0,  5,  6,  7,  0,  0,  0,  5,  6,  7,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[ 35, 36, 37,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[ 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39 ]
+		];
+		this.world = [
+			[ 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40 ],
+			[ 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40 ],
+			[ 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40 ],
+			[ 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40 ],
+			[ 40, 40, 40, 14, 13, 14, 40, 40, 40, 40, 40 ],
+			[ 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40 ],
+			[ 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40 ],
+			[ 40, 40, 14, 14, 14, 14, 14, 40, 40, 40, 15 ],
+			[ 40, 40, 40, 40, 40, 40, 40, 40, 40, 15, 15 ],
+			[ 40, 40, 40, 40, 40, 40, 40, 40, 15, 15, 15 ],
+			[ 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39 ]
+		];
+		this.foreground = [
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  1,  2,  3,  0,  0,  0,  1,  2,  3,  0 ],
+			[  0,  5,  6,  7,  0,  0,  0,  5,  6,  7,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  0,  0, 14, 13, 14,  0,  0,  0,  0,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+			[  0,  0, 14, 14, 14, 14, 14,  0,  0,  0, 15 ],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0, 15, 15 ],
+			[ 35, 36, 37,  0,  0,  0,  0,  0, 15, 15, 15 ],
+			[ 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39 ]
+		];
 	}
 
 	preload() {
 		this.load.image('ground', 'assets/sprites/platform.png');
 		this.load.spritesheet('ship', 'assets/sprites/anim_ship_spin.png', { frameWidth: 192, frameHeight: 63 });
-		
+		this.load.image('tiles', 'assets/sprites/super-mario-tiles.png');
 	}
 
 	create() {
@@ -18,7 +58,19 @@ export class SimpleScene extends Phaser.Scene {
 		this.cursors = this.input.keyboard.createCursorKeys();
 
 		this.platforms = this.physics.add.staticGroup();
-    	this.platforms.create(400, 568, 'ground').refreshBody();
+		this.platforms.create(400, 568, 'ground').refreshBody();
+		
+		this.belowMap = this.make.tilemap({ data: this.background, tileWidth: 32, tileHeight: 32 });
+		this.belowTiles = this.belowMap.addTilesetImage('tiles');
+		this.worldMap = this.make.tilemap({ data: this.world, tileWidth: 32, tileHeight: 32 });
+  		this.worldTiles = this.worldMap.addTilesetImage('tiles');
+		//this.layer = this.map.createStaticLayer(0, this.tiles, 0, 0);
+
+		//this.belowLayer = this.belowMap.createStaticLayer(0, this.belowTiles, 0, 0);
+  		this.worldLayer = this.worldMap.createStaticLayer(0, this.worldTiles, 0, 0);
+		//this.aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+		  
+		this.worldLayer.setCollision([13,14,39]);
 
 		this.player = this.physics.add.sprite(400, 300, 'ship');
 		//player.body.setGravity(0,0);
@@ -42,6 +94,7 @@ export class SimpleScene extends Phaser.Scene {
 		this.cameras.main.startFollow(this.player);
 
 		this.physics.add.collider(this.player, this.platforms, this.HitGround, null, this);
+		this.physics.add.collider(this.player, this.worldLayer, this.HitGround, null, this);
 	}
 
 	update() {
