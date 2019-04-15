e00ec29558b72f604cd7cae4bad2352733624188
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-04-14 21:40:50 -0400

    break create() up

--- app/scenes/SimpleScene.js
+++ app/scenes/SimpleScene.js
@@ -4,6 +4,9 @@ export class SimpleScene extends Phaser.Scene {
 		super({
 			key: 'SimpleScene'
 		});
+
+		this.player = null;
+		this.worldLayer = null;
 	}
 
 	preload() {
@@ -18,42 +21,18 @@ export class SimpleScene extends Phaser.Scene {
 		this.physics.world.setBounds(0, 0, 800, 32*100);
 		this.cameras.main.setBounds(this.physics.world.bounds.x, this.physics.world.bounds.y, this.physics.world.bounds.width, this.physics.world.bounds.height);
 
-		this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-
 		this.cursors = this.input.keyboard.createCursorKeys();
 
-		const map = this.make.tilemap({ key: "map" });
-		const tileset = map.addTilesetImage("Super Mario Tiles", "tiles");
+		this.worldLayer = this.InitTileMaps();
 
-		const belowLayer = map.createStaticLayer("Background", tileset, 0, 0);
-		const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
-		  
-		worldLayer.setCollisionByProperty({ collides: true });
-
-		this.player = this.physics.add.sprite(400, 300, 'ship');
-		//player.body.setGravity(0,0);
-		//this.player.body.allowGravity = false;
-		this.player.setActive(true);
-		this.player.body.setGravity(0,-100);
-		this.player.body.setAllowDrag(true);
-		this.player.body.setDrag(70, 70);
-		this.player.body.setFriction(0.7, 0);
-		this.player.setCollideWorldBounds(true);
-
-		this.anims.create({
-			key: 'spin',
-			frames: this.anims.generateFrameNumbers('ship', { start: 0, end: 3 }),
-			frameRate: 10,
-			repeat: -1
-		});
-
-		this.player.anims.play('spin', true);
+		this.InitPlayerAnims();
+		this.InitPlayerObject();
+		
 
 		//the camera will follow the player in the world
 		this.cameras.main.startFollow(this.player);
 
-		this.physics.add.collider(this.player, this.platforms, this.HitGround, null, this);
-		this.physics.add.collider(this.player, worldLayer, this.HitGround, null, this);
+		this.physics.add.collider(this.player, this.worldLayer, this.HitGround, null, this);
 	}
 
 	update() {
@@ -83,6 +62,40 @@ export class SimpleScene extends Phaser.Scene {
 		}
 
 
+	}
+
+	InitTileMaps(){
+		const map = this.make.tilemap({ key: "map" });
+		const tileset = map.addTilesetImage("Super Mario Tiles", "tiles");
+
+		const belowLayer = map.createStaticLayer("Background", tileset, 0, 0);
+		const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+		  
+		worldLayer.setCollisionByProperty({ collides: true });
+
+		return worldLayer;
+	}
+
+	InitPlayerObject(){
+		this.player = this.physics.add.sprite(400, 300, 'ship');
+		//player.body.setGravity(0,0);
+		//this.player.body.allowGravity = false;
+		this.player.setActive(true);
+		this.player.body.setGravity(0,-100);
+		this.player.body.setAllowDrag(true);
+		this.player.body.setDrag(70, 70);
+		this.player.body.setFriction(0.7, 0);
+		this.player.setCollideWorldBounds(true);
+		this.player.anims.play('spin', true);
+	}
+
+	InitPlayerAnims(){
+		this.anims.create({
+			key: 'spin',
+			frames: this.anims.generateFrameNumbers('ship', { start: 0, end: 3 }),
+			frameRate: 10,
+			repeat: -1
+		});
 	}
 
 	HitGround(){
